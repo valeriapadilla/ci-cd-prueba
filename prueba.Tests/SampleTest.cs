4b6e3043e78638badeae5da7f3ee482909e375a3
valeriapadilla <vpadillahoyos@gmail.com>
2025-03-09 22:03:45 -0500

    first commit

---
+++ prueba.Tests/SampleTest.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace prueba.Tests;
+
+public class SampleTests
+{
+    [Fact]
+    public void TestAlwaysPasses()
+    {
+        Assert.True(true);
+    }
+}
---
+++ prueba.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace prueba.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
